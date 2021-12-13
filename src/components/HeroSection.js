718c8833bcc29f29bd28cc3729dfb70385fb32b6
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-12-13 12:53:09 +0100

    improve styling and responsiveness

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <video src='/videos/video-4.mp4' autoPlay loop muted />
       <h1>SAMMY WYMER</h1>
       <p>Front-end developer</p>
      <span> <button className="projects-button">Projects</button>
--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -10,31 +10,31 @@ export default function ProjectCards() {
         <div className='project-cards-wrapper'>
         <ul className="project-cards-items">
        <ProjectCardsItem 
-       src="/images/img-1.jpg"
+       src="/images/weather-original.png"
        text='Weather app made with React and OpenWeather API.'
        label="Weather-React"/>
         <ProjectCardsItem 
-       src="/images/img-1.jpg"
-       text='Weather app made with React and OpenWeather API.'
-       label="Weather-React"/>
+       src="/images/dictionary-app-image.png"
+       text='Dictionary app made with React and 2 APIs.'
+       label="Dictionary-app"/>
         <ProjectCardsItem 
-       src="/images/img-1.jpg"
-       text='Weather app made with React and OpenWeather API.'
-       label="Weather-React"/>
+       src="/images/weather-app-image.png"
+       text='Weather app made using Vanilla Javascript and OpenWeather API.'
+       label="Weather-Javascript"/>
         </ul>
         <ul className="project-cards-items">
        <ProjectCardsItem 
-       src="/images/img-1.jpg"
-       text='Weather app made with React and OpenWeather API.'
-       label="Weather-React"/>
+       src="/images/tandem-app-about.png"
+       text='Landing page for undersTandem app.'
+       label="App landing page"/>
         <ProjectCardsItem 
-       src="/images/img-1.jpg"
-       text='Weather app made with React and OpenWeather API.'
-       label="Weather-React"/>
+       src="/images/seaglass-image.png"
+       text='A website for seaglass art pieces.'
+       label="Seaglass art website"/>
         <ProjectCardsItem 
-       src="/images/img-1.jpg"
-       text='Weather app made with React and OpenWeather API.'
-       label="Weather-React"/>
+       src="/images/pokemon-pic.jpg"
+       text='Weather app made with React and OpenWeather API that shows the weather in Pokemon.'
+       label="Weather-React: Pokemon"/>
         </ul>
         </div>
         </div>
