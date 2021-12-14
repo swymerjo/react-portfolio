e4eeab6ab3a0e5a2c7e25d9d8bec272b9bef8d86
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-12-14 16:16:20 +0100

    add image meta tag

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -9,32 +9,32 @@ export default function ProjectCards() {
         <div className='project-cards-container'>
         <div className='project-cards-wrapper'>
         <ul className="project-cards-items">
-       <ProjectCardsItem 
+       <a className="project-a-link" target="_blank" rel="noreferrer" href="https://trusting-mahavira-0d29ce.netlify.app"><ProjectCardsItem 
        src="/images/weather-original.png"
        text='Weather app made with React and OpenWeather API.'
-       label="Weather-React"/>
-        <ProjectCardsItem 
+       label="Weather-React"/></a>
+        <a className="project-a-link" target="_blank" rel="noreferrer" href="https://happy-payne-677245.netlify.app"><ProjectCardsItem 
        src="/images/dictionary-app-image.png"
        text='Dictionary app made with React and 2 APIs.'
-       label="Dictionary-app"/>
-        <ProjectCardsItem 
+       label="Dictionary-app"/></a>
+       <a className="project-a-link" target="_blank" rel="noreferrer" href="https://hardcore-fermat-72e5f3.netlify.app"> <ProjectCardsItem 
        src="/images/weather-app-image.png"
        text='Weather app made using Vanilla Javascript and OpenWeather API.'
-       label="Weather-Javascript"/>
+       label="Weather-Javascript"/></a>
         </ul>
         <ul className="project-cards-items">
-       <ProjectCardsItem 
+       <a className="project-a-link" target="_blank" rel="noreferrer" href="https://silly-shaw-c8aa88.netlify.app"><ProjectCardsItem 
        src="/images/tandem-app-about.png"
        text='Landing page for undersTandem app.'
-       label="App landing page"/>
-        <ProjectCardsItem 
+       label="App landing page"/></a>
+        <a className="project-a-link" target="_blank" rel="noreferrer" href="https://sleepy-dijkstra-f527ec.netlify.app"><ProjectCardsItem 
        src="/images/seaglass-image.png"
        text='A website for seaglass art pieces.'
-       label="Seaglass art website"/>
-        <ProjectCardsItem 
+       label="Seaglass art website"/></a>
+        <a className="project-a-link" target="_blank" rel="noreferrer" href="https://adoring-kepler-effbaf.netlify.app"><ProjectCardsItem 
        src="/images/pokemon-pic.jpg"
        text='Weather app made with React and OpenWeather API that shows the weather in Pokemon.'
-       label="Weather-React: Pokemon"/>
+       label="Weather-React: Pokemon"/></a>
         </ul>
         </div>
         </div>
