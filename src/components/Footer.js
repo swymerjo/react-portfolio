5ba4846c70abd1f6ade224d8e5ea0fb953d6371b
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-12-14 14:53:54 +0100

    add social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,11 @@ function Footer() {
           <h3 id="contact-email">sammyjowymerpsych@gmail.com</h3> 
         </section>
         <section className='footer-social-icons'>
-        <i className='fab fa-twitter social-icons' />    
-        <i className='fab fa-github social-icons' />    
-        <i className='fab fa-linkedin social-icons' />    
-        <i className='fab fa-instagram social-icons' />     
+
+        <a href="https://twitter.com/SammyWymer"><i className='fab fa-twitter social-icons' /></a>    
+        <a href="https://github.com/swymerjo"><i className='fab fa-github social-icons' /></a>  
+        <a href="https://www.linkedin.com/in/sammy-jo-wymer-6391b2205/"><i className='fab fa-linkedin social-icons' /></a>  
+        <a href="https://www.instagram.com/sammyjowymer/"><i className='fab fa-instagram social-icons' /></a>    
         </section>
         </div>
     )
