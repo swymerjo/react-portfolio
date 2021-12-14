e5245c2138e662bd7941abdb65aa5ba4042e1596
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-12-14 14:55:52 +0100

    add target blank to links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,10 @@ function Footer() {
         </section>
         <section className='footer-social-icons'>
 
-        <a href="https://twitter.com/SammyWymer"><i className='fab fa-twitter social-icons' /></a>    
-        <a href="https://github.com/swymerjo"><i className='fab fa-github social-icons' /></a>  
-        <a href="https://www.linkedin.com/in/sammy-jo-wymer-6391b2205/"><i className='fab fa-linkedin social-icons' /></a>  
-        <a href="https://www.instagram.com/sammyjowymer/"><i className='fab fa-instagram social-icons' /></a>    
+        <a href="https://twitter.com/SammyWymer" target="_blank"><i className='fab fa-twitter social-icons' /></a>    
+        <a href="https://github.com/swymerjo" target="_blank"><i className='fab fa-github social-icons' /></a>  
+        <a href="https://www.linkedin.com/in/sammy-jo-wymer-6391b2205/" target="_blank"><i className='fab fa-linkedin social-icons' /></a>  
+        <a href="https://www.instagram.com/sammyjowymer/" target="_blank"><i className='fab fa-instagram social-icons' /></a>    
         </section>
         </div>
     )
